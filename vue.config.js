ecd596ef0db4fa961e958b04ba06678643f69b93
lvxingchen <1063614453@qq.com>
2019-01-14 21:02:10 +0800

    config dist path

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,22 @@
-// module.exports = {
-//     devServer:{
-//         proxy:{
-//             '/api':{
-//                 target:'https://wd9872222184vuzzhq.wilddogio.com/',
-//                 changeOrigin:true,
-//                 ws:true,
-//                 pathRewrite:{
-//                     '^/api':''
-//                 }
-//             }
-//         }
-//     }
-// }
+module.exports = {
+    baseUrl: process.env.NODE_ENV === 'production'
+    ? './'
+    : '/',
+    // 输出文件目录
+    outputDir: 'dist', // 默认dist
+    // 用于嵌套生成的静态资产（js,css,img,fonts）目录
+    // assetsDir: '',
+    // 指定生成的 index.html 的输出路径 (相对于 outputDir)。也可以是一个绝对路径
+    indexPath: 'index.html', // Default: 'index.html'
+    filenameHashing: true,
+    // 构建多页时使用
+    pages: undefined,
+    // eslint-loader是否在保存的时候检查
+    lintOnSave: true,
+    // 是否使用包含运行时编译器的Vue核心的构建
+    runtimeCompiler: false,
+    // 默认情况下 babel-loader 会忽略所有 node_modules 中的文件。如果你想要通过 Babel 显式转译一个依赖，可以在这个选项中列出来
+    transpileDependencies: [],
+    // 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建。
+    productionSourceMap: false,
+}
